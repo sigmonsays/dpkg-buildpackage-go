b10164e9fb90e42866714a0ec1c3aa289d266649
sig <sig@desktop2.lan>
2021-02-21 14:58:45 -0800

    seed dpkg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 export DEBIAN_FRONTEND=noninteractive
+export DEBCONF_NONINTERACTIVE_SEEN=true
+cat <<EOF | debconf-set-selections
+tzdata	tzdata/Areas select	America
+tzdata	tzdata/Zones/America	select	Los_Angeles
+tzdata	tzdata/Zones/Etc	select	UTC
+tzdata	tzdata/Zones/US	select
+EOF
+
 set -e
 # Set the install command to be used by mk-build-deps (use --yes for non-interactive)
 install_tool="apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes"
